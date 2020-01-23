89dc4a007b52935b8a8e0818d69b7f926b86f412
Jorma <jorma.rebane@gmail.com>
2020-01-23 18:00:52 +0200

    Added mamafile.py for correct static build on windows

---
+++ mamafile.py
@@ -0,0 +1,16 @@
+import mama
+class curl(mama.BuildTarget):
+    def dependencies(self):
+        pass
+
+    def configure(self):
+        self.add_cmake_options('BUILD_CURL_EXE=OFF', 'BUILD_TESTING=OFF',
+                               'CURL_STATICLIB=ON', 'CURL_STATIC_CRT=ON',
+                               'CURL_DISABLE_LDAP=ON', 'CURL_DISABLE_LDAPS=ON')
+
+    def package(self):
+        self.copy_built_file('lib/curl_config.h', 'include/curl')
+        self.export_include('include', build_dir=True)
+        self.export_libs('lib', ['libcurl.lib', '.a'])
+        if self.windows:
+            self.export_syslib('Ws2_32.lib')
